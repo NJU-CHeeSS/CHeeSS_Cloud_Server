6fdc74efb67d289d93717ab87ea2a3ba521aa9a2
Lili0820 <1179028267@qq.com>
2018-01-03 16:47:17 +0800

    连接hbase配置

---
+++ cloudserver/src/test/java/edu/nju/cheess/cloudserver/HBaseTest.java
@@ -0,0 +1,12 @@
+package edu.nju.cheess.cloudserver;
+
+import edu.nju.cheess.cloudserver.dao.HBaseUtil;
+
+/**
+ * Created by CLL on 17/12/28.
+ */
+public class HBaseTest {
+    public static void main(String[] args){
+        HBaseUtil.listTables();
+    }
+}
